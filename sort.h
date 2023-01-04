b977f3c26f7bf8b7a6e609f7bf74b70408ab3c01
NeoDau <neodau@gmail.com>
2023-01-04 07:01:25 -0800

    README & sort

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+#endif
