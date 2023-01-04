50b9dbae4274f1b59d33631323683e0abc4aae76
NeoDau <neodau@gmail.com>
2023-01-04 07:44:34 -0800

    all project 0

--- sort.h
+++ sort.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
 
 #endif
